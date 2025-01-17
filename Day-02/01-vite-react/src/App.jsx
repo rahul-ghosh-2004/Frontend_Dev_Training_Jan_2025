4ce57334870b7e932c260d9672ba5f3c9d119b7b
rahul-ghosh-2004 <iamrahulghoshinfo@gmail.com>
2025-01-17 20:53:10 +0530

    Adding code files for day 2

---
+++ Day-02/01-vite-react/src/App.jsx
@@ -0,0 +1,10 @@
+import Temp from "./temp"
+function App() {
+  return (
+    <div>
+      <Temp />
+    </div>
+  )
+}
+
+export default App
---
+++ Day-02/01-vite-react/src/temp.jsx
@@ -0,0 +1,12 @@
+
+function Temp() {
+    return (
+        <div className=" h-screen w-screen flex items-center justify-center">
+            <div>
+                CODEXINTERN
+            </div>
+        </div>
+    )
+}
+
+export default Temp
